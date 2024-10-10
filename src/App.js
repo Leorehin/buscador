c66ffd5366ec87243cc6b4bf2531a73737b790a2
Leonardo Santander <leonardorehin@gmail.com>
2024-10-10 12:13:05 -0300

    commit da minha primeira tela

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import{useState} from 'react'
+import{FiSearch} from 'react-icons/fi'
+import './style.css'
+import api from './services/api'
 
 function App() {
+  //criando variaveis com useState
+  const [
+    // criando objeto input
+  input, setInput] = useState('');
+    //crio o objeto que a api restorna
+  const [cep, setCep] =useState({});
+
+  //criando uma funcao que gera um alerta
+  async function handleSearch(){
+    
+
+    if(input.length !== 8){
+        //janela de alerta
+      alert("PREENCHA UM CEP VALIDO")
+      setInput('')
+      return;
+    }
+    try {
+      const response = await api.get(input + '/json');
+      console.log(response.data)
+      setCep(response.data)
+      setInput("")
+
+    } catch {
+      alert("ops! Erro ao buscar o cep")
+      setInput('')
+    }
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //criando um espaço na tela configuravel
+    //o comando className atribui um nome ao campo que voce criou
+    <div className="container">
+        <h1 className='title'> Buscar Cep </h1>
+
+        <div className="containerInput">
+          <input
+          type="text"
+          placeholder="Digite seu Cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          />
+
+          <button className="buttonSearch" onClick={handleSearch}>
+            <FiSearch size="25" color='white'/>
+          </button>
+          </div>
+          
+          {Object.keys(cep).length > 0 && (
+            
+            <main className='main'>
+            <h2> CEP: {cep.cep}</h2>
+
+            <span> RUA: {cep.logradouro}</span>
+            <span>COMPLEMENTO: {cep.complemento}  </span>
+            <span> BAIRRO: {cep.bairro}</span>
+            <span> {cep.localidade} - {cep.uf}</span>
+          </main>
+
+          )}
+          
+
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//https://viacep.com.br/ws/41225690/json
+
+const api = axios.create({
+    baseURL:"https://viacep.com.br/ws/"
+});
+
+export default api;
+
